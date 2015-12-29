3ebc5a8cdf4b33ba43996b74885637f25d27f24a
Ionică Bizău <bizauionica@gmail.com>
2015-12-29 07:27:18 +0200

    :memo:

--- example/index.js
+++ example/index.js
@@ -2,16 +2,19 @@ const emojiFromText = require("../lib");
 
 // The results are ordered by the word count (in this case: home, sweet).
 console.log(emojiFromText("Home sweet home!"));
-// => [ Match {
-//     input: 'home',
-//     score: 1,
-//     emoji: { keywords: [Object], char: '👪', category: 'people' },
-//     emoji_name: 'family' },
-//   Match {
-//     input: 'sweet',
-//     score: 1,
-//     emoji: { keywords: [Object], char: '🍩', category: 'foodanddrink' },
-//     emoji_name: 'doughnut' } ]
+// => [ { name: 'home',
+//   count: 2,
+//   match: Match { input: 'home', score: 1, emoji: [Object], emoji_name: 'family' },
+//   score: 8 },
+// { name: 'sweet',
+//   count: 1,
+//   match:
+//    Match {
+//      input: 'sweet',
+//      score: 1,
+//      emoji: [Object],
+//      emoji_name: 'doughnut' },
+//   score: 5 } ]
 
 // Get only one emoji (the best match)
 console.log(emojiFromText("Hello World!", true).match.toString());
